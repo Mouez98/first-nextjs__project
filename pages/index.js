fbbe727164239f1d9e7420ce1d62abbe4fcbad5c
Mouez98 <mouez.matoui@gmail.com>
2022-02-26 10:26:05 +0100

    linking  between pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-
+import Link from 'next/link'
 
 function HomePage(){
-    return <h2>Home page</h2>
+    return <>
+    <h2>Home page</h2>
+    <ul>
+        <li>
+            <Link href="news/nextjs-is-a-great-framework">
+                NewtJs is a great framework
+            </Link>
+        </li>
+        <li>
+          something else
+        </li>
+    </ul>
+    </>
+   
 }
 
 export default HomePage;
